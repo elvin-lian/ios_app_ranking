286160f0dbdc9b6375b5a41cc625f8aa12ae47bc
Elvin <elvin.lian@foxmail.com>
2012-12-20 11:46:43 +0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IosAppRanking::Application.config.secret_token = '9964c92d3f99c6c6a08d36cec558c2eb6e33a861f627d9fd6574d876aa8cce580fc35c146f3e91ab218e71a9a1b41ba817d34abe4046df50df65d2dd7d9f8519'
